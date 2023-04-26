653dcfb2a45ae0b9c8e4e5ba7064b9e0422678ce
YuLim2 <jyulim0120@gmail.com>
2023-04-26 09:26:54 +0900

    FIX: content

--- site.config.js
+++ site.config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
+    name: "kingyulim",
+    image: "/img.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "YuRim Jin",
+    bio: "",
     email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    linkedin: "yulim-jin-2a411422b",
+    github: "yulim2",
+    instagram: "tjtan_lim",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
@@ -24,7 +22,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://kingyulim.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
